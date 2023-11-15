5d48c3832fc6fc84477a0456a6a254f790ac82f7
hallucinogenizer <tech@apollo-group.io>
2023-11-15 22:43:31 +0500

    social icons added

--- src/components/HeroSection/TextContent.tsx
+++ src/components/HeroSection/TextContent.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { ClassNameProp } from "../../lib/utils/utils";
 import { cn } from "../../lib/utils/cn";
+import GitHubIcon from "../../assets/github.svg";
+import LinkedInIcon from "../../assets/linkedin.svg";
+import FacebookIcon from "../../assets/facebook.svg";
+import TwitterIcon from "../../assets/twitter.svg";
 
 export const TextContent = () => (
   <div className="flex flex-col gap-2">
     <Hello className="mb-4" />
     <PersonName className="mb-2" />
     <ProfessionalTitles />
-    <DownloadResume />
+    <DownloadResume className="mt-10" />
+    <SocialIcons className="mt-8" />
   </div>
 );
 
@@ -44,8 +49,30 @@ const ProfessionalTitles = () => {
   );
 };
 
-const DownloadResume = () => (
-  <button className="mt-10 w-44 h-10 bg-gradient-to-b from-white/10 to-transparent hover:to-white/10 rounded text-light">
+const DownloadResume = ({ className }: { className?: string }) => (
+  <button
+    className={cn(
+      "w-44 h-10 bg-gradient-to-b from-white/10 to-transparent hover:to-white/10 rounded text-light",
+      className
+    )}
+  >
     Download Resume
   </button>
+);
+
+const SocialIcons = ({ className }: { className?: string }) => (
+  <div className={cn("flex gap-6", className)}>
+    <div className="w-10 h-10 bg-gradient-to-b from-white/20 to-white/10 hover:to-white/20 cursor-pointer rounded-full flex justify-center items-center">
+      <img src={GitHubIcon} width={24} className="w-6 h-auto" />
+    </div>
+    <div className="w-10 h-10 bg-gradient-to-b from-white/20 to-white/10 hover:to-white/20 cursor-pointer rounded-full flex justify-center items-center">
+      <img src={LinkedInIcon} width={24} className="w-6 h-auto" />
+    </div>
+    <div className="w-10 h-10 bg-gradient-to-b from-white/20 to-white/10 hover:to-white/20 cursor-pointer rounded-full flex justify-center items-center">
+      <img src={FacebookIcon} width={24} className="w-6 h-auto" />
+    </div>
+    <div className="w-10 h-10 bg-gradient-to-b from-white/20 to-white/10 hover:to-white/20 cursor-pointer rounded-full flex justify-center items-center">
+      <img src={TwitterIcon} width={24} className="w-6 h-auto" />
+    </div>
+  </div>
 );
