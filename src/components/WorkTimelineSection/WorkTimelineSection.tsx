65fb89128ce758c695da920a2b8f2a8c775e26d9
hallucinogenizer <tech@apollo-group.io>
2023-11-18 00:54:17 +0500

    hexagonal background pattern

--- src/components/WorkTimelineSection/WorkTimelineSection.tsx
+++ src/components/WorkTimelineSection/WorkTimelineSection.tsx
@@ -1,9 +1,17 @@
 import { SectionHeading } from "../common/SectionHeading";
+import HexagonalBackgroundPattern from "../../assets/hexagonal-pattern-background.svg";
 
 export default function WorkTimelineSection() {
   return (
-    <div className="flex flex-col gap-16 px-36">
-      <SectionHeading title={["My Work", "Experience"]} />
+    <div className="flex flex-col gap-16 px-36 relative overflow-clip">
+      <div className="relative z-10">
+        <SectionHeading title={["My Work", "Experience"]} />
+      </div>
+
+      {/* background hexagonal pattern */}
+      <div className="absolute">
+        <img src={HexagonalBackgroundPattern} className="opacity-20" />
+      </div>
     </div>
   );
 }
