074dff0a2d74939597a0ab013fdb00c303a7ec1f
hallucinogenizer <tech@apollo-group.io>
2023-11-23 23:08:49 +0500

    shortened headings for mobile

--- src/components/common/SectionHeading.tsx
+++ src/components/common/SectionHeading.tsx
@@ -1,7 +1,11 @@
 export const SectionHeading = ({ title }: { title: [string, string] }) => (
   <h1 className="leading-10 font-['Poppins']">
-    <span className="text-neutral-300 text-4xl font-medium">{title[0]}</span>
+    <span className="text-neutral-300 md:text-4xl text-xl font-medium">
+      {title[0]}
+    </span>
     <br />
-    <span className="text-cyan-500 text-6xl font-semibold ">{title[1]}</span>
+    <span className="text-cyan-500 md:text-6xl text-2xl font-semibold ">
+      {title[1]}
+    </span>
   </h1>
 );
